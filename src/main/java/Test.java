64630334738258e643f0491342f0048621ecff52
derya <derya.urcuk@ordina.nl>
2024-07-05 16:03:54 +0200

    Tweede commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello this is a test for GitHub Jenkins and Docker");
+        System.out.println("Hello stap2");
 
     }
 }
