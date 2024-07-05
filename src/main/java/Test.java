5d143560162209cca4c17618805812be239fd62b
derya <derya.urcuk@ordina.nl>
2024-07-05 16:14:24 +0200

    Derde commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello this is a test for GitHub Jenkins and Docker");
-        System.out.println("Hello stap2");
-
+        System.out.println("Hello step2");
+        System.out.println("Hello step3");
     }
 }
